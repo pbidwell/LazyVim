c2cf81386985a97c76ebd0da5d705b6d7833d49a
Ubuntu <deck@hippo.steamdeck>
2025-04-16 10:36:23 -0500

    Adding keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("n", "<C-Left>", "<CMD>vertical resize +2<CR>", { desc = "Grow window to the left" })
+vim.keymap.set("n", "<C-Right>", "<CMD>vertical resize -2<CR>", { desc = "Shrink window from the left" })
+
+vim.keymap.set("n", "<C-Up>", "<CMD>resize +2<CR>", { desc = "Grow window upwards" })
+vim.keymap.set("n", "<C-Down>", "<CMD>resize -2<CR>", { desc = "Shrink window from the top" })
